37654c0f026785f2c0f852e353bd3a2ec05d0fa1
webdorado <webdorado@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-06-07 13:43:06 +0000

    remove web-dorado.com link from frontend
    git-svn-id: https://plugins.svn.wordpress.org/folder-menu-vertical/tags/2.0@723991 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ folder_menu_vertrical.php
@@ -0,0 +1,578 @@
+<?php 
+
+/*
+Plugin Name: Wordpress Menu
+Plugin URI: http://web-dorado.com/
+Version: 2.0
+Author: http://web-dorado.com/
+License: GNU/GPLv3 http://www.gnu.org/licenses/gpl-3.0.html
+*/
+$addflashonce=1;
+//add to head javascript
+function my_custom_js()
+{
+global $addflashonce;
+$Menu_Name=get_option('Menu_Name');
+
+if($Menu_Name !='- Select Menu -' && $Menu_Name)
+{
+	echo  ' <script type="text/javascript" src="'.plugins_url( 'js/AC_RunActiveContent.js' , __FILE__ ).'"></script>
+ 			<script type="text/javascript" src="'.plugins_url( 'js/functions.js' , __FILE__ ).'"></script>';
+}
+}
+
+
+// hook for adding js code
+add_action('wp_head', 'my_custom_js',0);
+
+
+
+//display font end
+function front_end_vertical($content)  
+{ 
+$url_swf=plugins_url( '' , __FILE__ );
+global $addflashonce;
+$wrand=mt_rand();
+global $post;
+$from_top=get_option('Distance_from_top').'px';
+$Menu_Name=get_option('Menu_Name');
+$Menu_Font=get_option('Menu_Font');
+$positoin=get_option('Position');
+$mh_after_head = did_action( 'wp_enqueue_scripts' );
+if($Menu_Name !='- Select Menu -' && $addflashonce==1 && $Menu_Name && ($mh_after_head==1))
+{
+	$addflashonce=0;
+$FC_frontend=<<<HERE
+       <div id="foldermenuverticalContainer" onmouseover="onmouseover_vertical();" onmouseout="onmouseout_vertical();" style="position:$positoin; left:0px; top:$from_top; overflow:hidden;width:50px; z-index:1000; height:1500px;">
+
+	<style type="text/css">
+	#foldermenuverticalContainer embed {max-width: initial;}
+	</style>
+<script language="javascript">
+
+	if (typeof(AC_FL_RunContent)=="undefined") {
+
+		alert("This page requires AC_RunActiveContent.js.");
+
+	} else {
+
+		AC_FL_RunContent(
+
+			'codebase', 'http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=9,0,0,0',
+
+			'width', '1000',
+
+			'height', '3000',
+
+			'src', '$url_swf/Vertical$Menu_Font?wdrand=372906355',
+
+			'quality', 'high',
+
+			'pluginspage', 'http://www.macromedia.com/go/getflashplayer',
+
+			'align', 'left',
+
+			'play', 'true',
+
+			'loop', 'true',
+
+			'scale', 'showall',
+
+			'wmode', 'transparent',
+
+			'devicefont', 'false',
+
+			'id', 'FolderMenuVerticalElem',
+
+			'name', 'FolderMenuVerticalElem',
+
+			'menu', 'true',
+
+			'allowFullScreen', 'false',
+
+			'allowScriptAccess','always',
+
+			'movie', '$url_swf/Vertical$Menu_Font?wdrand=1018799407',
+
+			'salign', 't',
+
+			'flashvars', 'xmlUrl=$url_swf/Folder_vertical.php?wdrand=1566406044'
+
+			);
+
+			
+
+	if(typeof(FolderMenuVerticalElem)=="undefined") 
+
+		initFlash_Moz_vertical();
+
+	else
+
+		initFlash_vertical();
+
+		
+
+		}
+
+
+
+	
+
+function onmouseover_vertical()
+
+{
+
+if(typeof(FolderMenuVerticalObj)!="undefined")
+
+	{
+
+		FolderMenuVerticalObj.TOtrigger=true;
+
+		FolderMenuVerticalObj.setHeightAndWidthTO();
+
+	}
+
+}
+
+	
+
+function onmouseout_vertical()
+
+{
+
+if(typeof(FolderMenuVerticalObj)!="undefined")
+
+	{
+
+		FolderMenuVerticalObj.TOtrigger=false;
+
+	}
+
+}
+
+	
+
+function initFlash_Moz_vertical()
+
+{
+
+if(typeof(document.FolderMenuVerticalElem.getWidth)=='function')
+
+	FolderMenuVerticalObj=new FolderMenuVertical("foldermenuverticalContainer",document.FolderMenuVerticalElem,"FolderMenuVerticalObj");
+
+else {setTimeout("initFlash_Moz_vertical()",500);}
+
+}
+
+
+
+function initFlash_vertical()
+
+{
+
+if(typeof(FolderMenuVerticalElem.getWidth)=='function')
+
+	FolderMenuVerticalObj=new FolderMenuVertical("foldermenuverticalContainer",FolderMenuVerticalElem,"FolderMenuVerticalObj");
+
+else {setTimeout("initFlash_vertical()",500);}
+
+}
+
+</script>
+
+<noscript>
+
+
+
+</noscript>
+
+</div>
+
+<script language="javascript">
+
+	var foldermenuverticalContainerDiv = document.getElementById('foldermenuverticalContainer');
+
+	document.body.appendChild(foldermenuverticalContainerDiv);
+
+</script> 
+HERE;
+?><?php
+echo $FC_frontend;
+ $contenttt=$FC_frontend.$content;
+return $content;
+
+}
+
+else
+{
+ return $content;
+}
+
+}
+
+
+
+//// add frot end hook
+add_action('the_content', 'front_end_vertical'); 
+
+
+function front_end_vertical_fotter()  
+{ 
+$url_swf=plugins_url( '' , __FILE__ );
+global $addflashonce;
+$wrand=mt_rand();
+global $post;
+$from_top=get_option('Distance_from_top').'px';
+$Menu_Name=get_option('Menu_Name');
+$Menu_Font=get_option('Menu_Font');
+$positoin=get_option('Position');
+if($Menu_Name !='- Select Menu -'  && $Menu_Name && $addflashonce==1)
+{
+	$addflashonce=0;
+$FC_frontend=<<<HERE
+       <div id="foldermenuverticalContainer" onmouseover="onmouseover_vertical();" onmouseout="onmouseout_vertical();" style="position:$positoin; left:0px; top:$from_top; overflow:hidden;width:50px; z-index:1000; height:1500px;">
+
+<script language="javascript">
+
+	if (typeof(AC_FL_RunContent)=="undefined") {
+
+		alert("This page requires AC_RunActiveContent.js.");
+
+	} else {
+
+		AC_FL_RunContent(
+
+			'codebase', 'http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=9,0,0,0',
+
+			'width', '1000',
+
+			'height', '3000',
+
+			'src', '$url_swf/Vertical$Menu_Font?wdrand=372906355',
+
+			'quality', 'high',
+
+			'pluginspage', 'http://www.macromedia.com/go/getflashplayer',
+
+			'align', 'left',
+
+			'play', 'true',
+
+			'loop', 'true',
+
+			'scale', 'showall',
+
+			'wmode', 'transparent',
+
+			'devicefont', 'false',
+
+			'id', 'FolderMenuVerticalElem',
+
+			'name', 'FolderMenuVerticalElem',
+
+			'menu', 'true',
+
+			'allowFullScreen', 'false',
+
+			'allowScriptAccess','always',
+
+			'movie', '$url_swf/Vertical$Menu_Font?wdrand=1018799407',
+
+			'salign', 't',
+
+			'flashvars', 'xmlUrl=$url_swf/Folder_vertical.php?wdrand=1566406044'
+
+			);
+
+			
+
+	if(typeof(FolderMenuVerticalElem)=="undefined") 
+
+		initFlash_Moz_vertical();
+
+	else
+
+		initFlash_vertical();
+
+		
+
+		}
+
+
+
+	
+
+function onmouseover_vertical()
+
+{
+
+if(typeof(FolderMenuVerticalObj)!="undefined")
+
+	{
+
+		FolderMenuVerticalObj.TOtrigger=true;
+
+		FolderMenuVerticalObj.setHeightAndWidthTO();
+
+	}
+
+}
+
+	
+
+function onmouseout_vertical()
+
+{
+
+if(typeof(FolderMenuVerticalObj)!="undefined")
+
+	{
+
+		FolderMenuVerticalObj.TOtrigger=false;
+
+	}
+
+}
+
+	
+
+function initFlash_Moz_vertical()
+
+{
+
+if(typeof(document.FolderMenuVerticalElem.getWidth)=='function')
+
+	FolderMenuVerticalObj=new FolderMenuVertical("foldermenuverticalContainer",document.FolderMenuVerticalElem,"FolderMenuVerticalObj");
+
+else {setTimeout("initFlash_Moz_vertical()",500);}
+
+}
+
+
+
+function initFlash_vertical()
+
+{
+
+if(typeof(FolderMenuVerticalElem.getWidth)=='function')
+
+	FolderMenuVerticalObj=new FolderMenuVertical("foldermenuverticalContainer",FolderMenuVerticalElem,"FolderMenuVerticalObj");
+
+else {setTimeout("initFlash_vertical()",500);}
+
+}
+
+</script>
+
+<noscript>
+
+
+
+</noscript>
+
+</div>
+
+<script language="javascript">
+
+	var foldermenuverticalContainerDiv = document.getElementById('foldermenuverticalContainer');
+
+	document.body.appendChild(foldermenuverticalContainerDiv);
+
+</script> 
+HERE;
+?><?php
+echo $FC_frontend;
+}
+
+}
+
+
+
+//// add frot end hook
+add_action('wp_footer', 'front_end_vertical_fotter'); 
+
+
+
+//add menu hook
+add_action('admin_menu', 'Vertical_menu');
+
+// function creatin menu
+function Vertical_menu(){
+  add_menu_page('Folder Menu', 'Folder Menu', 'manage_options', 'Vertical_menu', 'Manage_Vertical_menu');
+}
+
+
+
+function Manage_Vertical_menu()
+{
+	echo '<script src="'.plugins_url( 'jscolor/jscolor.js' , __FILE__ ).'"></script>';
+	echo '	<div style="text-align:left;font-size:16px;padding:20px; padding-right:50px">
+		<a href="http://web-dorado.com/files/fromFolderMenu.php?from=wp" target="_blank" style="color:red; text-decoration:none;">
+		<img src="http://web-dorado.com/images/header_paypal.png" border="0" alt="www.web-dorado.com" width="215"><br />
+		&nbsp;&nbsp;&nbsp;&nbsp;Get the full version<br />
+		&nbsp;&nbsp;&nbsp;&nbsp;(without a link to our site)
+		</a>
+	</div>';
+	echo "<h2>" . __( 'Folder Menu Parametrs', 'oscimp_trdom' ) . "</h2>";
+	if($_POST['Fold_men_vert'] == 'YY') {  
+        //update display
+        $Menu_Name = $_POST['Menu_Name'];  
+        update_option('Menu_Name', $Menu_Name);  
+		
+		 $Position = $_POST['Position'];  
+        update_option('Position', $Position);
+		
+		 $Distance_from_top = $_POST['Distance_from_top'];  
+        update_option('Distance_from_top', $Distance_from_top);
+		
+		 $Menu_Background_Color = $_POST['Menu_Background_Color'];  
+        update_option('Menu_Background_Color', $Menu_Background_Color);
+		
+		 $Menu_Text_Color = $_POST['Menu_Text_Color'];  
+        update_option('Menu_Text_Color', $Menu_Text_Color);
+		
+		 $Menu_Hover_Color = $_POST['Menu_Hover_Color'];  
+        update_option('Menu_Hover_Color', $Menu_Hover_Color);
+		
+		 $Submenu_Text_Color = $_POST['Submenu_Text_Color'];  
+        update_option('Submenu_Text_Color', $Submenu_Text_Color);
+		
+		 $Submenu_Hover_Color = $_POST['Submenu_Hover_Color'];  
+        update_option('Submenu_Hover_Color', $Submenu_Hover_Color);
+        
+         $Menu_Font = $_POST['Menu_Font'];  
+        update_option('Menu_Font', $Menu_Font);
+		
+        ?>  
+        <div class="updated"><p><strong><?php _e('Options saved.' ); ?></strong></p></div>  
+        <?php  
+    }
+	
+	
+	 else {  
+        //Normal page display  
+   		 $Menu_Name=get_option('Menu_Name');  
+         $Position=get_option('Position');
+         $Distance_from_top=get_option('Distance_from_top');  
+         $Menu_Background_Color=get_option('Menu_Background_Color');
+         $Menu_Text_Color=get_option('Menu_Text_Color'); 
+         $Menu_Hover_Color=get_option('Menu_Hover_Color'); 
+         $Submenu_Text_Color=get_option('Submenu_Text_Color');
+         $Submenu_Hover_Color=get_option('Submenu_Hover_Color'); 
+		 $Menu_Font=get_option('Menu_Font');
+    }  
+?>
+
+<form method="post" action="<?php echo str_replace( '%7E', '~', $_SERVER['REQUEST_URI']); ?>">
+<input type="hidden" value="YY" name="Fold_men_vert" />
+<table class="form-table">
+<tr>
+<th>Menu Name</th>
+<td>
+<select name="Menu_Name" style="width:200px">
+<option value="- Select Menu -"  <?php if($Menu_Name==='- Select Menu -') echo ' selected="selected"' ?>>- Select Menu -</option>
+<?php			$menu_items=wp_get_nav_menus();
+			foreach($menu_items as $menu_item ){
+?>
+<option value="<?php echo $menu_item->name ?>" <?php if($Menu_Name===$menu_item->name) echo ' selected="selected"' ?>><?php echo $menu_item->name ?></option>
+<?php } ?>
+</select>
+</td>
+</tr>
+<tr>
+<th>Position</th>
+<td>
+<select name="Position" style="width:200px">
+<option value="Fixed" <?php if($Position==='Fixed') echo ' selected="selected"'; ?>>Fixed</option>
+<option value="Absolute" <?php if($Position==='Absolute') echo ' selected="selected"'; ?>>Absolute</option>
+</select>
+</td>
+</tr>
+
+<tr>
+<th>Distance from top</th>
+<td>
+<input type="text" value="<?php echo $Distance_from_top ?>" name="Distance_from_top" />
+</td>
+</tr>
+
+<tr>
+<th>Menu Background Color</th>
+<td>
+<input type="text" value="<?php echo $Menu_Background_Color ?>" name="Menu_Background_Color" class="color" />
+</td>
+</tr>
+
+<tr>
+<th>Menu Text Color</th>
+<td>
+<input type="text" value="<?php echo $Menu_Text_Color ?>" name="Menu_Text_Color" class="color" />
+</td>
+</tr>
+
+<tr>
+<th>Menu Hover Color</th>
+<td>
+<input type="text" value="<?php echo $Menu_Hover_Color ?>" name="Menu_Hover_Color" class="color" />
+</td>
+</tr>
+
+<tr>
+<th>Submenu Text Color</th>
+<td>
+<input type="text" value="<?php echo $Submenu_Text_Color ?>" name="Submenu_Text_Color" class="color" />
+</td>
+</tr>
+
+<tr>
+<th>Submenu Hover Color</th>
+<td>
+<input type="text" value="<?php echo $Submenu_Hover_Color ?>" name="Submenu_Hover_Color" class="color" />
+</td>
+</tr>
+
+<tr>
+<th>Menu Font</th>
+<td>
+<select  name="Menu_Font" >
+<?php echo $Menu_Font ?>
+<option value="Verdana" <?php if($Menu_Font=='Verdana') echo 'selected="selected"'?>>Verdana</option>
+<option value="Lucida" <?php if($Menu_Font=='Lucida') echo 'selected="selected"'?>>Lucida</option>
+<option value="Georgia" <?php if($Menu_Font=='Georgia') echo 'selected="selected"'?>>Georgia</option>
+</select>
+
+</td>
+</tr>
+</table>
+<p class="submit">
+<input type="submit" value="Save"  />
+</p>
+
+
+ </form>
+<?php 
+?>
+
+
+
+
+<?php
+}
+function folder_activate()
+{
+if(!get_option('Menu_Background_Color'))
+{
+        add_option('Menu_Name','- Select Menu -');  
+        add_option('Position', 'fixed');
+        add_option('Distance_from_top', '80');  
+        add_option('Menu_Background_Color', '00ADEF');
+        add_option('Menu_Text_Color', 'FFFFFF'); 
+        add_option('Menu_Hover_Color', '174287'); 
+        add_option('Submenu_Text_Color', 'FFFFFF');
+        add_option('Submenu_Hover_Color', '174287'); 
+        add_option('Menu_Font', 'Verdana');
+}
+	
+}
+register_activation_hook( __FILE__, 'folder_activate' );
+?>
---
+++ readme.txt
@@ -0,0 +1,121 @@
+=== WordPress Menu Vertical ===
+Contributors: webdorado
+Donate link: http://web-dorado.com/products/wordpress-menu-vertical.html
+Tags: menu, flash, flash menu, folder, folder menu, vertical menu, fixed position, fixed menu, sidebar menu, flash effects, customizable menu, customizable colors
+Requires at least: 3.0
+Tested up to: 3.5
+Stable tag: 2.0
+
+Folder Menu Vertical is a flash dynamic menu module for your WordPress 3.0+ website, designed to meet your needs and preferences.
+
+== Description ==
+
+####[WordPress vertical menu](http://web-dorado.com/products/wordpress-menu-vertical.html)
+
+Folder Menu Vertical is a flash dynamic menu module for your WordPress 3.0+ website, designed to meet your needs and preferences. 
+
+[Demo](http://wpdemo.web-dorado.com/folder-menu-vertical/)  
+[Support Forum](http://web-dorado.com/forum/folder-menu.html)  
+[FAQ](http://web-dorado.com/forum/folder-menu/4-folder-menu-faq.html)  
+
+#### Main features:
+*   Possibility to customize the menu using various parameters
+*   Possibility to fix the position of the menu during the scrolling.
+*   Nice Flash effects
+*   Interface fully integrated with WordPress administration, which allows to use [WordPress vertical menu](http://web-dorado.com/products/wordpress-menu-vertical.html) with new or existing menus of WordPress.
+
+This WordPress Menu plugin makes the Menu very convenient and easy-to-use.
+
+Now you can organize your website pages more easily and present user-friendly websites to your customers and visitors with our WordPress dropdown menu plugin.
+
+== Installation ==
+
+####Thank you for your interest in [WordPress vertical menu](http://web-dorado.com/products/wordpress-menu-vertical.html).
+
+After downloading the ZIP file  
+1.   Login to your WordPress site and head over the 'Plugins' menu  
+2.   Click 'Add New'  
+3.   Choose the 'Upload' option  
+4.   Upload the downloaded ZIP file.  
+5.   Once it is complete, activate the plugin.  
+
+Once the plugin is activated,  you'll notice a menu on the left called "Folder Menu". Here you can select the menu from the list of your [WordPress menus](http://wordpress.org/extend/plugins/folder-menu-vertical/), and also change various parameters.
+
+== Frequently Asked Questions ==
+
+[Visit FAQ section on web-dorado.com](http://web-dorado.com/forum/folder-menu/4-folder-menu-faq.html)
+= 1. Can I customize the colors and font of the Folder Menu? =    
+    
+Yes. You can customize all colors of the Folder Menu and also can select the font of the Folder Menu from the parameters.    
+    
+    
+= 2. Can I fix the position of the Folder Menu while scrolling? =    
+    
+Yes, there is an option in [WordPress vertical menu](http://web-dorado.com/products/wordpress-menu-vertical.html) to set the position fixed or unfixed while scrolling.     
+    
+    
+= 3. On which WordPress versions does Folder Menu work? =    
+    
+Folder Menu has versions for WordPress 3.0 +.    
+    
+    
+= 4. Is there an option to put images in Folder Menu? =    
+    
+No, there is no such an option in Folder Menu.    
+    
+    
+= 5. I bought Folder Menu but I have not received an email for confirmation and for downloading. =    
+    
+Usually confirmation e-mail arrives during a minute after the purchase of Folder Menu. If the e-mail of the purchase of Folder Menu does not arrive during half an hour, please contact us. We will do our best to solve the problem as soon as possible and will also send the purchased Folder Menu by e-mail.    
+    
+    
+= 6. Folder Menu does not work or works in an improper way. What should I do? =    
+    
+We did our best to have a universal product which will work on any browser, template and will not conflict with other extensions. As any case of such kind of a problem  is unique, please contact us giving the link of the website where the problem has occurred and [WordPress vertical menu](http://web-dorado.com/products/wordpress-menu-vertical.html) is installed. We will try to answer within 24 hours.    
+    
+    
+= 7. I wrote an e-mail concerning Folder Menu and have not received a respond. =    
+    
+Do not worry. Our team will contact you during 24 hours after your question and answer to all question concerning Folder Menu.    
+    
+    
+= 8. I have installed Folder Menu, but can't see it on the front end. =    
+    
+Please make sure that you have activated the [WordPress menu plugin](http://wordpress.org/extend/plugins/folder-menu-vertical/), go to the menu item on the left called "Folder Menu" and select the menu from the list of your [WordPress dropdown menus](http://wordpress.org/extend/plugins/folder-menu-vertical/).    
+    
+
+== Screenshots ==
+1.  [WordPress vertical menu](http://web-dorado.com/products/wordpress-menu-vertical.html)
+2.  [WordPress vertical menu](http://web-dorado.com/products/wordpress-menu-vertical.html)
+3.  [WordPress menu plugin](http://wordpress.org/extend/plugins/folder-menu-vertical/)
+4.  Folder Menu Parameters
+
+== Folder Menu user manual ==         
+= Step 1:  Installing folder menu. =   
+
+1.1 Minimum requirements of using [WordPress vertical menu](http://web-dorado.com/products/wordpress-menu-vertical.html).                        
+*	Wordpress 3.0+  
+*	PHP 5.x  
+*	MySQL 5.x  
+1.2 Perform a new installation of Folder Menu.  
+
+1.	Log in to the administrator panel to install Foler Menu.  
+2.	Go to Plugins Add > New > Upload.  
+3.	Click 'Choose file' ('Browse') and select the Folder Menu zip file.  
+4.	Click 'Install Now' button to install Folder Menu.  
+5.	If the installation of Folder Menu succeeded you will see the message in the picture. If any problem occurs with the installation of [WordPress vertical menu](http://web-dorado.com/products/wordpress-menu-vertical.html), please contact us info@web-dorado.com.  
+6.	Click 'Activate Plugin' button for activating the [WordPress Menu Plugin](http://wordpress.org/extend/plugins/folder-menu-vertical/).  
+
+= Step 2: Creating a menu =  
+2.1 On the left menu select Folder Menu  
+Menu Name. Select a menu from the existing list.  
+Position of a menu. Define the position of the menu during the scrolling.  
+Distance from top of the menu. Define the distance of the menu from the top.  
+Menu Background Color. Change the background color of the menu.  
+Menu Text Color. Change the color of the menu text.  
+Menu Hover Color. Choose what color the menu gets when being pointed on by the mouse cursor.  
+The menu's submenu Text Color. Change the color of the submenu text.  
+The menu's submenu Hover Color. Choose what color the submenu gets when being pointed on by the mouse cursor.  
+Menu Font. Choose the font of the menu.  
+Press the 'Save' button to save the menu.  
+
